7400c819b8d2bdc0b39247d88b70b91ee769e2af
AZZAMAF <aafazzam@gmail.com>
2026-04-14 06:08:26 +0700

    Update SectionDownload

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -2,6 +2,7 @@
 
 import DefaultLayout from '../../components/Layouts/DefaultLayout/index.jsx'
 import Jumbotron from '../../components/Moduls/Jumbotron/index.jsx'
+import SectionDownload from '../../components/Moduls/SectionContents/SectionDownload.jsx'
 import SectionEnjoy from '../../components/Moduls/SectionContents/SectionEnjoy.jsx'
 
 function Landing(){
@@ -10,6 +11,7 @@ function Landing(){
     <DefaultLayout>
       <Jumbotron/>
       <SectionEnjoy/>
+      <SectionDownload/>
     </DefaultLayout>
   )
 }
